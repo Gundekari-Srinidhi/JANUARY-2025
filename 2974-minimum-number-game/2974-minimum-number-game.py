e1779934212d796ccfd85905974e9eaadb776e35
Gundekari-Srinidhi <gundekarisrinidhi@gmail.com>
2026-01-28 21:59:56 +0530

    Time: 0 ms (100%), Space: 19.4 MB (12.24%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        arr=[]
+        nums.sort()
+        i=0
+        while i <len(nums):
+            arr.append(nums[i+1])
+            arr.append(nums[i])
+            i+=2
+        return arr
+
+        
